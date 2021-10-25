4f87ce648611de451f3ad73866792c9b0d18b680
welschoff <h.welschoff@gmx.de>
2021-10-25 20:40:07 +0200

    test

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import './style.css';
+import { createElement } from '.lib/elements';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+async function renderApp() {
+  const appElement = document.querySelector('#app');
+
+  const headerElement = createElement('header', { className: 'header' }, [
+    createElement('h1', {
+      textContent: ' Game of Thrones',
+    }),
+  ]);
+
+  appElement.append(appElement);
+}
+renderApp();
