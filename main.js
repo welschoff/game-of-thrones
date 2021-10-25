5f556bec689ed19d558d0f2e607066ee620cac0d
welschoff <h.welschoff@gmx.de>
2021-10-25 20:58:20 +0200

    test

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
+import { createElement } from './lib/elements';
 import './style.css';
-import { createElement } from '.lib/elements';
 
 async function renderApp() {
   const appElement = document.querySelector('#app');
 
-  const headerElement = createElement('header', { className: 'header' }, [
-    createElement('h1', {
-      textContent: ' Game of Thrones',
-    }),
-  ]);
+  const headerElement = createElement(
+    'header',
+    {
+      className: 'header',
+    },
+    [
+      createElement('h1', {
+        textContent: 'Game of Thrones',
+      }),
+    ]
+  );
 
-  appElement.append(appElement);
+  const mainElement = createElement(
+    'main',
+    {
+      className: 'main',
+    },
+    characterCards
+  );
+
+  appElement.append(headerElement, mainElement);
 }
+
 renderApp();
